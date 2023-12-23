705fc42ea24058054743090746859d5944bc81d8
yangfan <279303278@qq.com>
2023-12-23 17:57:00 +0800

    开启暴露aop代理对象

--- system-boot/src/main/java/com/ptu/devCloud/Application.java
+++ system-boot/src/main/java/com/ptu/devCloud/Application.java
@@ -3,11 +3,13 @@ package com.ptu.devCloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableAspectJAutoProxy(exposeProxy=true,proxyTargetClass=true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
