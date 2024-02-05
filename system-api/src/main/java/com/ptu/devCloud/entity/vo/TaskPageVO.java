2138d7cff30191c7e7877008ec38244fe044c8d6
yangfan <279303278@qq.com>
2024-02-05 15:16:03 +0800

    update

--- system-api/src/main/java/com/ptu/devCloud/entity/vo/TaskPageVO.java
+++ system-api/src/main/java/com/ptu/devCloud/entity/vo/TaskPageVO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * TaskPageVO
@@ -14,14 +15,20 @@ public class TaskPageVO extends PageVO implements Serializable {
     /** 任务名称 */
     private String taskName;
 
-    /** 任务类型 */
-    private String taskType;
+    /** 任务类型-多选 */
+    private List<String> taskTypeList;
 
-    /** 任务状态 */
-    private String taskStatus;
+    /** 任务状态-多选 */
+    private List<String> taskStatusList;
 
     /** 任务编号 */
     private String serialNumber;
+
+    /** 当前操作人 */
+    private Long currentOperator;
+
+    /** 参与者（单个用户） */
+    private String participant;
 
     /** 创建人 */
     private Long createBy;
